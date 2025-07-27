e6f817946909a204b84a646b588a29a1d4b93a23
Kayla George <kaylatrinitygeorge@gmail.com>
2025-07-27 15:31:11 -0400

    base spring app

---
+++ src/main/java/solutions/york/budgetbookbackend/BudgetBookBackendApplication.java
@@ -0,0 +1,13 @@
+package solutions.york.budgetbookbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetBookBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetBookBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/solutions/york/budgetbookbackend/BudgetBookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package solutions.york.budgetbookbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetBookBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
