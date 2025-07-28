0758fd56266448add38bee4176ea01ba0f7db90d
Kayla George <kaylatrinitygeorge@gmail.com>
2025-07-28 12:33:27 -0400

    fixing customer response (replacing setters with getters)

--- src/main/java/solutions/york/budgetbookbackend/dto/customer/CustomerResponse.java
+++ src/main/java/solutions/york/budgetbookbackend/dto/customer/CustomerResponse.java
@@ -18,20 +18,20 @@ public class CustomerResponse {
         this.phoneNumber = customer.getPhoneNumber();
     }
 
-    // SETTERS
-    public void setId(Long id) {
-        this.id = id;
+    // GETTERS
+    public Long getId() {
+        return id;
     }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public String getFirstName() {
+        return firstName;
     }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public String getLastName() {
+        return lastName;
     }
-    public void setEmail(String email) {
-        this.email = email;
+    public String getEmail() {
+        return email;
     }
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+    public String getPhoneNumber() {
+        return phoneNumber;
     }
 }
