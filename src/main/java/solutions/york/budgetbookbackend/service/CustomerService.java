e0c321833eabc083df0cced7b8b231eaf5a68ba3
Kayla George <kaylatrinitygeorge@gmail.com>
2025-08-04 16:31:49 -0400

    creating default categories and fixing bug with signup

--- src/main/java/solutions/york/budgetbookbackend/service/CustomerService.java
+++ src/main/java/solutions/york/budgetbookbackend/service/CustomerService.java
@@ -4,15 +4,19 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 import solutions.york.budgetbookbackend.dto.customer.CustomerRequest;
 import solutions.york.budgetbookbackend.dto.customer.CustomerResponse;
+import solutions.york.budgetbookbackend.model.Category;
 import solutions.york.budgetbookbackend.model.Customer;
+import solutions.york.budgetbookbackend.repository.CategoryRepository;
 import solutions.york.budgetbookbackend.repository.CustomerRepository;
 
 @Service
 public class CustomerService {
     private final CustomerRepository customerRepository;
+    private final CategoryRepository categoryRepository;
 
-    public CustomerService(CustomerRepository customerRepository) {
+    public CustomerService(CustomerRepository customerRepository, CategoryRepository categoryRepository) {
         this.customerRepository = customerRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     public void validateCustomerRequest(CustomerRequest request) {
@@ -36,11 +40,36 @@ public class CustomerService {
             }
         }
         Customer customer = customerRepository.save(new Customer(request));
+        createDefaultCategories(customer);
         return new CustomerResponse(customer);
     }
 
+    public void createDefaultCategories(Customer customer) {
+        categoryRepository.save(new Category(customer, "Housing"));
+        categoryRepository.save(new Category(customer, "Utilities & Bills"));
+        categoryRepository.save(new Category(customer, "Groceries"));
+        categoryRepository.save(new Category(customer, "Dining & Takeout"));
+        categoryRepository.save(new Category(customer, "Clothing & Accessories"));
+        categoryRepository.save(new Category(customer, "Beauty & Personal Care"));
+        categoryRepository.save(new Category(customer, "Health & Insurance"));
+        categoryRepository.save(new Category(customer, "Education"));
+        categoryRepository.save(new Category(customer, "Automotive & Transportation"));
+        categoryRepository.save(new Category(customer, "Home & Garden"));
+        categoryRepository.save(new Category(customer, "Computers & Electronics"));
+        categoryRepository.save(new Category(customer, "Sports & Outdoors"));
+        categoryRepository.save(new Category(customer, "Toys & Games"));
+        categoryRepository.save(new Category(customer, "Baby & Kids"));
+        categoryRepository.save(new Category(customer, "Pet Care"));
+        categoryRepository.save(new Category(customer, "Entertainment & Activities"));
+        categoryRepository.save(new Category(customer, "Travel"));
+        categoryRepository.save(new Category(customer, "Financial"));
+        categoryRepository.save(new Category(customer, "Work Expenses"));
+        categoryRepository.save(new Category(customer, "Donations"));
+        categoryRepository.save(new Category(customer, "Miscellaneous"));
+    }
+
     public Customer findByEmail(String email) {
-        return customerRepository.findByEmail(email).orElseThrow(() -> new IllegalArgumentException("Email not found"));
+        return customerRepository.findByEmail(email).orElse(null);
     }
 
 }
