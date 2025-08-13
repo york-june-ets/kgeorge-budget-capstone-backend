9230be9996895e9c5edfa3c38c44e0ad69c370b6
Kayla George <kaylatrinitygeorge@gmail.com>
2025-08-13 13:06:35 -0400

    fixing count query

--- src/main/java/solutions/york/budgetbookbackend/repository/TransactionRepository.java
+++ src/main/java/solutions/york/budgetbookbackend/repository/TransactionRepository.java
@@ -84,10 +84,10 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
         """,
         countQuery=
         """
-        SELECT COUNT (*) (
-            SELECT DISTINCT t.id
-            FROM allocation a
-            JOIN transaction t ON a.transaction_id = t.id
+        SELECT COUNT(DISTINCT transaction_id) FROM (
+            SELECT t.id AS transaction_id
+            FROM transaction t
+            LEFT JOIN allocation a ON a.transaction_id = t.id
             WHERE t.customer_id = :customerId
             AND t.archived = false
             AND (:accountId IS NULL OR t.account_id = :accountId)
@@ -95,35 +95,23 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
             AND (:dateFrom IS NULL OR t.date >= CAST(:dateFrom AS DATE))
             AND (:dateTo IS NULL OR t.date <= CAST(:dateTo AS DATE))
             AND (:categoryId IS NULL OR a.category_id = :categoryId)
-    
+        
             UNION ALL
-    
-            SELECT DISTINCT
-                -1 * (t.id * 10000 + ROW_NUMBER() OVER (PARTITION BY t.id ORDER BY future_date.date)),
-                future_date.date AS date,
-                t.description,
-                t.account_id,
-                t.type,
-                t.amount,
-                t.repeat_unit,
-                t.repeat_interval,
-                t.archived,
-                t.customer_id
+        
+            SELECT t.id AS transaction_id
             FROM transaction t
             JOIN generate_series(
                 CAST(:dateFrom AS DATE),
                 CAST(:dateTo AS DATE),
                 (t.repeat_interval || ' ' || t.repeat_unit)::interval
             ) AS future_date(date) ON future_date.date > t.date
-            LEFT JOIN allocation a ON a.transaction_id = t.id
             WHERE t.repeat_unit IS NOT NULL
             AND t.repeat_interval IS NOT NULL
             AND t.customer_id = :customerId
             AND t.archived = false
             AND (:accountId IS NULL OR t.account_id = :accountId)
             AND (:transactionType IS NULL OR t.type = CAST(:transactionType AS VARCHAR))
-            AND (:categoryId IS NULL OR a.category_id = :categoryId)
-        ) AS transactions
+        ) AS transaction
         """,
     nativeQuery = true)
     Page<Transaction> findTransactionsWithFilters(
